2d998813a7da0050dda05834de31d7614ac34537
ArpitRathi1 <rathi844@gmail.com>
2024-06-18 13:51:16 +0530

    update datatype

--- 3-datatype.js
+++ 3-datatype.js
@@ -14,3 +14,12 @@ const student = {
 console.log(student)               // output : { name: 'Arpit', age: 27, marks: 90, isPass: true }
 console.log(typeof(student))       // output : object
 console.log(typeof(student.age))   // output : number
+
+// To access any value.
+console.log(student["name"])        // "" is required
+// or
+console.log(student.name)
+
+// To update a value.
+student["marks"] = 89;
+console.log(student.marks)
