ac3fb89036d34751b77e63b276ceca7bbaa8501a
ArpitRathi1 <rathi844@gmail.com>
2024-06-18 13:50:17 +0530

    update datatype

--- 3-datatype.js
+++ 3-datatype.js
@@ -3,3 +3,10 @@ console.log(typeof(num));     // output : number
 
 let str = "Arpit Rathi";
 console.log(typeof(str));     // output : string
+
+const student = {
+	name : "Arpit",
+	age : 27,
+	marks : 90,
+	isPass : true
+}
