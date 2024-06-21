5c79a22fb75fd8c39bebb2962472abcd08fe5bb3
ArpitRathi1 <rathi844@gmail.com>
2024-06-21 12:16:17 +0530

    update operator.js

--- 4-operators.js
+++ 4-operators.js
@@ -8,3 +8,15 @@ console.log(a,"+",b,"=",a+b);
 // unary operator
 c++;
 console.log(c)    // output : 9
+
+// Assignment operator
+let d = 15;      // Assign
+d += 10
+console.log(d)   // output : 25
+
+// Loose equlily
+let num1 = 5;
+let num2 = "5";
+console.log(num1 == num2)   // output : true
+// Strict equality
+console.log(num1 === num2)  // output : false 
