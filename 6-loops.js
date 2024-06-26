10d4fa2a1a053ac301e2f0e2243a2208fef9f5a2
ArpitRathi1 <rathi844@gmail.com>
2024-06-26 11:51:52 +0530

    update loop

--- 6-loops.js
+++ 6-loops.js
@@ -4,3 +4,11 @@ for (let i = 1; i <= 5; i++){
 	console.log(i,"Hello World")
 }
 
+// Sum of 1 to n
+
+let n = 5;
+let sum = 0;
+for (let i = 0; i <= n; i++){
+	sum = sum + i;
+}
+console.log(sum)         // output : 15
