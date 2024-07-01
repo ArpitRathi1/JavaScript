92c78050f050f67233057d828d7ddd250b428ad1
ArpitRathi1 <rathi844@gmail.com>
2024-07-01 22:37:33 +0530

    update array

--- 8-array.js
+++ 8-array.js
@@ -6,3 +6,7 @@ console.log(info.length)     // output : 4
 
 // typeof - In JS array is also a special type is object.
 console.log(typeof info)    // output : object
+
+// Accessing Array Elements
+console.log(info[0])        // output : Arpit
+console.log(info[4])        // output : undefined
