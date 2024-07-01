73fa14c329de840ab072636993da343c4afbb775
ArpitRathi1 <rathi844@gmail.com>
2024-07-01 22:37:03 +0530

    update array

--- 8-array.js
+++ 8-array.js
@@ -3,3 +3,6 @@ let info = ["Arpit",20,10,"Ankit"];
 
 // Length of an array (length is a property)
 console.log(info.length)     // output : 4
+
+// typeof - In JS array is also a special type is object.
+console.log(typeof info)    // output : object
