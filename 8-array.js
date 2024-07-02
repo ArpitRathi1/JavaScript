66376613713fcc0cf18391a6dd00904df5dab189
ArpitRathi1 <rathi844@gmail.com>
2024-07-02 06:40:09 +0530

    update array

--- 8-array.js
+++ 8-array.js
@@ -15,6 +15,7 @@ console.log(info[4])        // output : undefined
 info[1] = 22 
 console.log(info)          // output : ['Arpit', 22, 10, 'Ankit']
 
+// looping over an array
 // Using for-loop
 for (let i = 0; i<=(info.length-1); i++){
 	console.log(info[i])                       // output : Arpit 22 10 Ankit
