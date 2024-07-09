4af711fca806f6d0db3c3262c5e803c0ce09c084
ArpitRathi1 <rathi844@gmail.com>
2024-07-09 10:40:54 +0530

    add function

---
+++ 9-function.js
@@ -0,0 +1,8 @@
+// Creating a function
+
+function myFunction(msg){          // here msg is parameter. 
+	console.log(msg)
+}
+
+// Calling function
+myFunction("Arpit")                // here Arpit is argument.
