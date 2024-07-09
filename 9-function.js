e8f79743f01e37ebb43d7fe660f7c011ab366ade
ArpitRathi1 <rathi844@gmail.com>
2024-07-09 10:41:20 +0530

    update function

--- 9-function.js
+++ 9-function.js
@@ -6,3 +6,11 @@ function myFunction(msg){          // here msg is parameter.
 
 // Calling function
 myFunction("Arpit")                // here Arpit is argument.
+
+// function to add to values.
+function sum(x, y){                // x and y are sum function's local variables
+	console.log(arguments)         // return object output : { '0': 1, '1': 2 }
+	s = x + y
+	return s                       // return will always return only one value
+}
+console.log(sum(1,2))              // output : 3
