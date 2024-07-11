c413d35e6412776318fde9eb4e49fed989840e91
ArpitRathi1 <rathi844@gmail.com>
2024-07-11 11:49:34 +0530

    update object

--- 10-objects.js
+++ 10-objects.js
@@ -16,3 +16,18 @@ console.log(student.grade)          // output : A
 
 // To invoke a method
 student.printMarks()                // output : Marks : 93
+
+// To remove a property
+delete student.grade
+console.log(student.grade)          // output : undefined
+
+// Object cloning - There are 3 ways to clone an object.
+// 1. Iteration-
+let src_1 = {
+	name : "Tommy"
+}
+let dest_1 = {}
+for (let key in src_1){
+	dest_1[key] = src_1[key]
+}
+console.log(dest_1)               // output : { name: 'Tommy' }
