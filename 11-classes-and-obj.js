3350c5a67024f714c471b3de4f4fd21a4a47d68c
ArpitRathi1 <rathi844@gmail.com>
2024-07-13 10:49:14 +0530

    update class and obj

---
+++ 11-classes-and-obj.js
@@ -0,0 +1,17 @@
+// Creating our own prototype.
+const employee = {
+	calcTax(){
+		console.log("tax is 10%")
+	}
+}
+
+const karan = {
+	salary : 25000
+}
+
+const arjun = {
+	salary : 50000,
+	calcTax(){
+		console.log("tax is 20%")
+	}
+}
