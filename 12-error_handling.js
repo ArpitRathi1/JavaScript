19cc111c25a594276f943e71ac9efd20ed734a60
ArpitRathi1 <rathi844@gmail.com>
2024-07-15 11:46:20 +0530

    update error handleing

--- 12-error_handling.js
+++ 12-error_handling.js
@@ -5,4 +5,11 @@ console.log("b =",b)
 console.log("a + b =",a+b)
 console.log("a + b =",a+b)
 console.log("a + b =",a+b)
-
+try{
+	console.log("a + b =",a+c)
+}
+catch(err){
+	console.log(err)
+}
+console.log("a + b =",a+b)
+console.log("a + b =",a+b)
