a5eaa3dc765710a32547f9aeeb3f2b0a4ba846d8
ArpitRathi1 <rathi844@gmail.com>
2024-07-16 10:15:57 +0530

    update hoisting

--- 13-Hoisting.js
+++ 13-Hoisting.js
@@ -1,3 +1,5 @@
 // Hoisting is a phenomina in JS by which we can access varible and functions even before we initialise it.
 // It is because of execution context.
-
+// Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope(to the top of the current script or the current function).
+// Variables defined with let and const are hoisted to the top of the block, but not initialized.
+// Meaning: The block of code is aware of the variable, but it cannot be used until it has been declared.
